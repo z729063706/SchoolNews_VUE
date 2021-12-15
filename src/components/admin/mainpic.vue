5ea97aca6c438a937135d1a7272de6a61c0f490f
z729063706 <z729063706@github.com>
2021-12-15 18:08:21 +0800

    添加轮播图管理

--- src/components/admin/mainpic.vue
+++ src/components/admin/mainpic.vue
@@ -1,18 +1,34 @@
 <template>
   <div class>
     <div class="content">
+      <el-button size="mini" type="default" @click="dialoadAddView= true">添加新图片</el-button>
+      <el-dialog class="dialog-addUser" title="添加新图片" :visible.sync="dialoadAddView">
+        <div class="layout-row">
+          <h3>图片地址</h3>
+          <el-input v-model="picurl" placeholder="请输入图片地址"></el-input>
+        </div>
+        <div class="layout-row">
+          <h3>详细描述</h3>
+          <el-input v-model="piccontent" placeholder="请输入图片描述"></el-input>
+        </div>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialoadAddView = false">取 消</el-button>
+          <el-button type="primary" @click="picAdd">确 定</el-button>
+        </div>
+      </el-dialog>
       <el-table
         :data="picList"
+        :row-class-name="isVaild"
         style="width: 100%"
       >
         <el-table-column prop="id" label="轮播图id"></el-table-column>
         <el-table-column prop="url" label="图片地址"></el-table-column>
         <el-table-column prop="content" label="图片描述"></el-table-column>
-        <el-table-column prop="inserttime" label="添加时间"></el-table-column>
+        <el-table-column prop="inserttime" label="日期" width="180"></el-table-column>
         <el-table-column label="操作">
-          <template>
-            <el-button size="mini" type="default" disabled>修改(不可操作)</el-button>
-            <el-button size="mini" type="danger" disabled>删除(不可操作)</el-button>
+          <template slot-scope="scope">
+            <el-button size="mini" type="danger" @click="picDelete(scope.row.id)">删除</el-button>
+            <el-button size="mini" type="danger" @click="picSwitch(scope.row.id)">{{Vaild(scope.row.vaild)}}</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -29,7 +45,10 @@ export default {
       msg: "Hello 管理员管理",
       username: "",
       password: "",
+      picurl:"",
+      piccontent:"",
       role: "newsadmin",
+      dialoadAddView: false,
       picList:{}
     };
   },
@@ -37,82 +56,53 @@ export default {
   computed: {},
   watch: {},
   methods: {
-    handleAdd() {
+    Vaild(v){
+      if (v == true){
+        return '禁用';
+      }
+      else{
+        return '启用';
+      }
+    },
+    isVaild({row, rowIndex}){
+      //console.log(row,rowIndex);
+      if (row.vaild === false) {
+          return 'inVaildRow';
+        }
+    },
+    picAdd() {
       let obj = {
-        _id: this.username,
-        password: this.password,
-        role: this.role
+        uname: global.user.uname,
+        upass: global.user.upass,
+        picurl: this.picurl,
+        piccontent: this.piccontent
       };
+      //console.log(obj);
       this.axios
-        .post("/admin/addAdmin", obj)
+        .post("/api/addpic", obj)
         .then(res => {
-          window.console.log(res);
-          if (res.data.msg == "添加成功") {
             this.$message({
               type: "success",
               message: "添加成功"
             });
             this.dialoadAddView = false;
-            this.$store.commit("addUser",obj)
-            this.username=""
-            this.password=""
-          } else {
-            this.$message({
-              type: "danger",
-              message: res.data.msg
+            this.axios.get("api/mainpic")
+            .then(res=>{
+              this.picList = res.data;
+              //console.log(this.picList);
             });
-          }
-        })
-        .catch(() => {});
-    },
-    userEdit(uid) {
-      //console.log(uid);
-      this.$prompt("请输入密码", "提示", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        inputPattern: /^(?![A-Z]+$)(?![a-z]+$)(?!\d+$)(?![\W_]+$)\S{6,16}$/,
-        inputErrorMessage:
-          "密码需要有小写字母、大写字母、数字、特殊符号的两种及以上并且密码长度在6到16位"
-      })
-        .then(({ value }) => {
-          let obj = {
-            id: uid,
-            newpass: value,
-            uname: global.user.uname,
-            upass: global.user.upass
-          };
-          console.log(obj);
-          this.axios
-            .post("/api/useredit", obj)
-            .then(res => {
-              if (res.data) {
-                this.$message({
-                  type: "success",
-                  message: "密码修改成功"
-                });
-              } else {
-                this.$message({
-                  type: "danger",
-                  message: "用户权限不足!"
-                });
-              }
-            })
-            .catch(() => {
-              this.$message({
-                type: "danger",
-                message: "用户权限不足!"
-              });
-            });
+            this.picList.splice(0, 1);
         })
         .catch(() => {
           this.$message({
-            type: "info",
-            message: "取消修改"
-          });
+              type: "error",
+              message: "系统错误"
+            });
         });
     },
-    userDelete(id) {
-      this.$confirm("此操作将永久删除该用户, 是否继续?", "提示", {
+    picDelete(id) {
+      //console.log(id);
+      this.$confirm("此操作将永久删除该图片, 是否继续?", "提示", {
         confirmButtonText: "确定",
         cancelButtonText: "取消",
         type: "warning"
@@ -121,20 +111,26 @@ export default {
           let obj = {
             uname: global.user.uname,
             upass: global.user.upass,
-            delid:id
+            delid: id
           };
-          this.axios.post("api/deluser",obj)
+          this.axios.post("api/delpic",obj)
           .then(() => {
-            this.axios.post("api/getuserlist?uname="+global.user.uname+"&upass="+global.user.upass)
+            this.axios.get("api/mainpic")
             .then(res=>{
-              this.userList = res.data;
-              //console.log(this.userList);
+              this.picList = res.data;
+              //console.log(this.picList);
             });
-            this.userList.splice(index, 1);
+            this.picList.splice(0, 1);
             this.$message({
               type: "success",
               message: "删除成功"
             });
+          })
+          .catch(() =>{
+            this.$message({
+            type: "error",
+            message: "服务异常!"
+          });
           })
         })
         .catch(() => {
@@ -144,12 +140,39 @@ export default {
           });
         });
     },
+  picSwitch(id){
+    let obj = {
+            uname: global.user.uname,
+            upass: global.user.upass,
+            switchid: id
+          };
+    this.axios.post("api/switchpic",obj)
+          .then(() => {
+            this.axios.get("api/mainpic")
+            .then(res=>{
+              this.picList = res.data;
+            });
+            this.picList.splice(0, 1);
+            this.$message({
+              type: "success",
+              message: "操作成功"
+            });
+          })
+          .catch(()=>{
+            this.$message({
+              type: "error",
+              message: "服务异常"
+            });
+          });
+
   },
+  },
+
   mounted() {
     this.axios.get("api/mainpic")
     .then(res=>{
       this.picList = res.data;
-      console.log(this.this.picList);
+      console.log(this.picList);
     });
   }
 };
@@ -162,4 +185,7 @@ export default {
 .el-select {
   width: 100%;
 }
+.el-table .inVaildRow {
+    background: #f0b9b7 !important ;
+  }
 </style>
--- src/components/admin/manage.vue
+++ src/components/admin/manage.vue
@@ -19,7 +19,6 @@
       <el-table-column label="操作">
         <template slot-scope="scope">
           <el-button size="mini" type="success" @click="handleView(scope.row.id)">查看</el-button>
-          <el-button size="mini" type="default" @click="handleEdit(scope.row.id)">编辑</el-button>
           <el-button size="mini" type="danger" @click="handleDelete(scope.row.id)">删除</el-button>
         </template>
       </el-table-column>
@@ -82,11 +81,6 @@ export default {
       //console.log(id)
       this.$router.push({
         path: "/readNews/"+id,
-      });
-    },
-    handleEdit(id) {
-      this.$router.push({
-        path: "/admin/edit/"+id,
       });
     },
     handleDelete(id) {
